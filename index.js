09916ff35aa0306887bcd2a5dfb263d6f49c6841
LDH0606 <46421300+leedongho0606@users.noreply.github.com>
2020-08-04 22:00:56 +0900

    v1.0.2 Update
    v1.0.2 Update
    html entity decode

--- index.js
+++ index.js
@@ -1,9 +1,9 @@
-const Discord = require("discord.js"), https = require("https"); // request 모듈은 개발이 중지되었으므로 가벼운(?) https 모듈을 사용
+const Discord = require("discord.js"), https = require("https"), html_entities = new (require('html-entities').AllHtmlEntities)();// request 모듈은 개발이 중지되었으므로 가벼운(?) https 모듈을 사용
 let dmdata; // 보낸 재난문자의 발송시간
 function webhooksend(title, info, time) {
-    new Discord.WebhookClient("WEBHOOK ID","WEBHOOK TOKEN")
-        .send({ "embeds": [{ "title": title, "color": 16711680, "description": info, "footer": { "text": "국민재난안전포털 | " + time, "icon_url": "https://raw.githubusercontent.com/leedongho0606/cp/master/img/logo_gov.png" }, }] });
-    console.log("전송됨: " + title + "\n" + info);
+    new Discord.WebhookClient("webhook id", "webhook token") // 아래코드에서는 html entity를 decode한다(부하줄이기 위함)
+        .send({ "embeds": [{ "title": title, "color": 16711680, "description": html_entities.decode(info), "footer": { "text": "국민재난안전포털 | " + time, "icon_url": "https://raw.githubusercontent.com/leedongho0606/cp/master/img/logo_gov.png" }, }] });
+    console.log("웹훅 전송됨: " + title + "\n" + info);
 }
 function getdisaster_message(callback) { // https: 443, http: 80
     let data = ""; // 받은 데이터 임시 저장용 변수
@@ -12,16 +12,23 @@ function getdisaster_message(callback) { // https: 443, http: 80
             data += Buffer.from(body).toString(); // 서버에서 나눠서 클라이언트로 전송하므로 data 변수에 받을때마다 넣음
         });
         res.on("end", () => { // 서버와의 연결이 종료되면
-            data = JSON.parse(data)[0]; // 최상위 재난문자
-            callback((data.SJ).substr(20), (data.CONT).replace(/&nbsp;/g, " ").replace(/&lt;/g, "<").replace(/&gt;/g, ">").replace(/&amp;/g,"&").replace(/&quot;/g,'"').replace(/&#035;/g, "#").replace(/&#039;/g, "'"), (data.SJ).substring(0, 19)); // 데이터 처리 다되면 콜백
+            try { // 재난문자 데이터를 JSON데이터화 성공하면
+                data = JSON.parse(data)[0]; // 최상위 재난문자
+                callback((data.SJ).substr(20), data.CONT, (data.SJ).substring(0, 19)); // 이코드에서는 html entity를 decode하지 않는다(부하줄이기 위함), 데이터 처리 다되면 콜백
+                //console.log("재난문자 불러오기 성공!\n",data);
+            } catch (e) { // 재난문자 데이터를 JSON데이터화 실패하면 오류로 인해 프로세스가 종료되는걸 막는다.
+                console.error("재난문자 불러오기 실패\n", e);
+                callback(null);
+            }
         });
     }).end(); // 서버 연결을 종료.
 }
 setInterval(() => {
     getdisaster_message((title, info, time) => {
+        if (!title) return;
         if (dmdata && dmdata != time) { // 이전에 보낸 재난문자가 아니라면
             webhooksend(title, info, time); // 웹훅 전송
         }// else if (!dmdata) // 이전에 보낸 재난문자라면
         dmdata = time; // 이전에 보낸 재난문자의 발송시간을 변수에 대입
     })
-},1000); // 1초마다 파싱
+}, 1000); // 1초마다 파싱
